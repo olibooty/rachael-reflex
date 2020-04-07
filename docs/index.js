9f7e66e402fd47e2de3d115f187aad0121607720
Oliver Booty <oliboots@gmail.com>
2020-04-07 17:42:26 +0100

    add docs folder for sample page

---
+++ docs/index.js
@@ -0,0 +1,79 @@
+const menuButton = document.querySelector('.menu-button');
+const scrollUpButton = document.querySelector('.scroll-up');
+const navList = [].slice.call(document.querySelector('.nav-list').children);
+const navListMobile = [].slice.call(document.querySelector('.nav-list--mobile ul').children);
+
+function hasClass(classList, className) {
+    const classes = [].slice.call(classList);
+    return classes.indexOf(className) !== -1;
+}
+
+function removeActiveClassFromLinks(navList) {
+    navList.forEach(function(el) {
+        el.children[0].classList.remove('active');
+    });
+}
+
+scrollUpButton.style.display = 'none';
+
+// scrolls page to the top when clicked
+function onClickScrollButton(e) {
+    e.preventDefault();
+
+    removeActiveClassFromLinks(navList);
+    removeActiveClassFromLinks(navListMobile);
+
+    document.body.scrollTop = 0; // for safari
+    document.documentElement.scrollTop = 0;
+    window.location = '#';
+
+    menuButton.classList.remove('open');
+}
+scrollUpButton.addEventListener('click', onClickScrollButton);
+
+// show/ hide scroll button
+function onScroll() {
+    if (document.body.scrollTop > 48 || document.documentElement.scrollTop > 48) {
+        scrollUpButton.style.display = 'block';
+    } else {
+        removeActiveClassFromLinks(navList);
+        removeActiveClassFromLinks(navListMobile);
+
+        scrollUpButton.style.display = 'none';
+    }
+}
+window.addEventListener('scroll', onScroll);
+
+function onClick(e) {
+    const classes = e.target.classList;
+
+    // toggle active links
+    if (classes.contains('link')) {
+        removeActiveClassFromLinks(navList);
+        removeActiveClassFromLinks(navListMobile);
+        classes.add('active');
+        menuButton.classList.remove('open');
+    }
+    // toggle mobile menu
+    if (classes.contains('menu-button')) {
+        menuButton.classList.toggle('open');
+    }
+}
+window.addEventListener('click', onClick);
+
+function onLoad() {
+    // toggle active links on load
+    const link = navList.filter(function(el) {
+        return el.children[0].hash === window.location.hash;
+    })[0];
+
+    if (link) {
+        link.children[0].classList.add('active');
+    }
+
+    // hide transform: translate animation on load
+    setTimeout(function() {
+        document.querySelector('.nav-list--mobile').classList.remove('hidden-on-load');
+    }, 200);
+}
+window.addEventListener('load', onLoad);
